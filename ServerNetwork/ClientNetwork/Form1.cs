0946a60c6309f96dd00401c07049106a334aef7c
markforest <toxerius7@gmail.com>
2018-09-06 11:14:20 +0300

    commit 1

---
+++ ServerNetwork/ClientNetwork/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientNetwork
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                s.Connect("127.0.0.1", 8005);
+                if (s.Connected)
+                {
+                    this.BackColor = Color.Green;
+                    String strSend = textBox1.Text;
+                    s.Send(Encoding.Unicode.GetBytes(strSend));
+                    byte[] buffer = new byte[1024];
+                    int l = 0;
+                    do
+                    {
+                        l = s.Receive(buffer);
+                        button1.Text += Encoding.Unicode.GetString(buffer, 0, l);
+                    } while (l > 0);
+                }
+                else
+                {
+                    MessageBox.Show("Error!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ ServerNetwork/ServerNetwork/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerNetwork
+{
+    class Program
+    {
+        static int port = 8005; //порт для приема входных запросов
+        static void Main(string[] args)
+        {
+            //получаем адреса для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            //создаем сокет
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                //связать сокет с локальной точкой, по которой будем принимать данные
+                listenSocket.Bind(ipPoint);
+                //начинаем прослушивание
+                listenSocket.Listen(10);
+                Console.WriteLine("Сервер запущен. Ожидание соединения ....");
+                while (true)
+                {
+                    Socket clientSocket = listenSocket.Accept();
+                    //Получение сообщения
+                    Console.WriteLine($"Ip: {clientSocket.RemoteEndPoint}");
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; //количество полученных байтов
+                    byte[] data = new byte[1024]; //буфер для получаемых данных
+                    do
+                    {
+                        bytes = clientSocket.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data));
+
+                    } while (clientSocket.Available>0);
+                    Console.WriteLine($"{DateTime.Now.ToShortTimeString()} {builder.ToString()}");
+                    //отправка ответа
+                    string message = "Ваше сообщение достовлено";
+                    data = Encoding.Unicode.GetBytes(message);
+                    clientSocket.Send(data);
+                    //закрываем клиентский сокет
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
